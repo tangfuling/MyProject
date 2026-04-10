4030b6bbfdcc67e9a16b304c075aa0fa5cc6d61f
tangfuling@chagent.com <tangfuling@ke.com>
2026-04-10 18:41:56 +0800

    feature:支持正式版部署

--- gzh-react/src/common/network/HttpConfig.ts
+++ gzh-react/src/common/network/HttpConfig.ts
@@ -1,6 +1,6 @@
 const isDebug = false;
 const DEBUG_BASE_URL = 'http://127.0.0.1:8081';
-const RELEASE_BASE_URL = 'https://api-gzh.niumatech.com';
+const RELEASE_BASE_URL = 'https://www.niumatech.com/api';
 
 function getBaseUrl() {
   return isDebug ? DEBUG_BASE_URL : RELEASE_BASE_URL;
